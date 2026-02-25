6d87ceb2c0c5688c178aa0ecaae2c47ad340dd32
Dhilipan31 <dhilipanpress@gmail.com>
2026-02-25 23:51:29 +0530

    init cmd 2

---
+++ src/test/java/com/example/emi_insight/EmiInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emi_insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmiInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
