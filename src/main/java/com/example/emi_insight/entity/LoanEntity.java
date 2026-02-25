46f878c7c5a9c1221441d58d156420a8453029a9
Dhilipan31 <dhilipanpress@gmail.com>
2026-02-26 00:12:37 +0530

    update load enitity

--- src/main/java/com/example/emi_insight/entity/LoanEntity.java
+++ src/main/java/com/example/emi_insight/entity/LoanEntity.java
@@ -1,6 +1,11 @@
 package com.example.emi_insight.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "loans")
@@ -9,4 +14,21 @@ public class LoanEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+    @Column(unique = true)
+    public String loanId;
+    public String name;
+    public Double principal;
+    public Double interest_rate;
+    public Integer tenure_months;
+    public Double emi;
+    public LocalDate startDate;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    public Timestamp created_at;
+    @UpdateTimestamp
+    public Timestamp updated_at;
+
+
+
 }
